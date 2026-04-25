733e9bcf02300832c3cdb325fb2b3f26637c8969
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:47:53 +0200

    modif tarifs avril 2026

--- data/pizzas/sucrees-salees.js
+++ data/pizzas/sucrees-salees.js
@@ -3,45 +3,45 @@ module.exports = [
     name: 'Exotique',
     description: 'Crème fraîche, ananas, lardons, magrets fumés, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   },
   {
     name: 'Montagnarde',
     description: 'Crème fraîche, chèvre, miel, lardons, oignons, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   },
   {
     name: 'Chef',
     description: 'Crème fraîche, saumon gralax, chèvre, ananas, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   },
   {
     name: 'Chicken Mooréa',
     description: 'Crème fraîche, poulets mariné aux épices, ananas, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   },
   {
     name: 'Baroussaise',
     description: 'Crème fraîche, pomme fruits, fromage de vache, lardons, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   }
 ]
