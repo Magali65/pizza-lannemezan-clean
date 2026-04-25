08aef256a5d840b215785f2f3e3507dab74ab32c
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:44:15 +0200

    modif tarifs avril 2026

--- data/pizzas/legumes.js
+++ data/pizzas/legumes.js
@@ -3,9 +3,9 @@ module.exports = [
     name: 'L’estivale',
     description: 'Tomate, légumes frais (suivant marché de la saison), mozzarella, pesto',
     prices: {
-      petite: 6.5,
-      moyenne: 11,
-      grande: 13
+      petite: 7.5,
+      moyenne: 12,
+      grande: 14
     }
   }
 ]
