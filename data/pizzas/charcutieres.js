2d339b16a74bc32d46c4576f6708d3cddbd855d8
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:40:31 +0200

    modif tarifs avril 2026

--- data/pizzas/charcutieres.js
+++ data/pizzas/charcutieres.js
@@ -3,18 +3,18 @@ module.exports = [
     name: 'Mixed',
     description: 'Crème fraîche, purée de poivrons, chorizo, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 11.0,
-      grande: 13.0
+      petite: 7.5,
+      moyenne: 12.0,
+      grande: 14.0
     }
   },
   {
     name: 'Charcutière',
     description: 'Tomate ou crème fraîche, 3 charcuteries, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 11.0,
-      grande: 13
+      petite: 7.5,
+      moyenne: 12.0,
+      grande: 14
     }
   }
 ]
