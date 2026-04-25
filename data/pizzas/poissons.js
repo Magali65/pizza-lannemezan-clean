33fa2ee3171d1868b0e04b2d1f228c527974ea96
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:44:56 +0200

    modif tarifs avril 2026

--- data/pizzas/poissons.js
+++ data/pizzas/poissons.js
@@ -3,18 +3,18 @@ module.exports = [
     name: 'Provençale',
     description: 'Tomate, pesto, poivrons, oignons, persillade, anchois, olives noires, mozzarella',
     prices: {
-      petite: 5.5,
-      moyenne: 10.5,
-      grande: 12.5
+      petite: 6.5,
+      moyenne: 11.5,
+      grande: 13.5
     }
   },
   {
     name: 'Océane',
     description: 'Crème fraîche, saumon gravlax, roquette, crème balsamique, mozzarella',
     prices: {
-      petite: 7,
-      moyenne: 13,
-      grande: 15
+      petite: 8,
+      moyenne: 14,
+      grande: 16
     }
   }
 ]
