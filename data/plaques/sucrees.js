5dfa2178268b0003ae59b5931f8631a4e607c04f
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:50:56 +0200

    modif tarifs avril 2026

--- data/plaques/sucrees.js
+++ data/plaques/sucrees.js
@@ -2,19 +2,19 @@ module.exports = [
   {
     name: 'Croustade aux pommes',
     prices: {
-      plaque: 39
+      plaque: 40
     }
   },
   {
     name: 'Tarte aux pommes compote',
     prices: {
-      plaque: 43
+      plaque: 44
     }
   },
   {
     name: 'Tarte aux pommes crème pâtissière',
     prices: {
-      plaque: 43
+      plaque: 44
     }
   }
 ]
