864780f250026aafdf4ec590a788d3c2403dd32b
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:51:28 +0200

    modif tarifs avril 2026

--- data/pizza-du-mois.js
+++ data/pizza-du-mois.js
@@ -3,7 +3,7 @@ module.exports = {
   description: 'Crème fraiche, Mozzarella, Écrasé de pommes de terre, Ventrêche, Lard de noir de bigorre, Fromage de vache.',
   image: 'mois_decembre.jpg',
   prices: {
-    moyenne: 13.50,
-    grande: 15.50
+    moyenne: 14.50,
+    grande: 16.50
   }
 }
