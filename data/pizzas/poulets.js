4183f2d63699ea64078262ebd93144bf964146d1
Magali65 <45878046+Magali65@users.noreply.github.com>
2026-04-25 19:45:40 +0200

    modif tarifs avril 2026

--- data/pizzas/poulets.js
+++ data/pizzas/poulets.js
@@ -3,27 +3,27 @@ module.exports = [
     name: 'Poulet basquaise',
     description: 'Tomate, poulet, poivrons, oignons, persillade, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   },
   {
     name: 'Poulet forestier',
     description: 'Crème fraîche, champignons de Paris, poulet, persillade, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   },
   {
     name: 'Poulet à l’indienne',
     description: 'Crème fraîche, poulet mariné aux épices, mozzarella',
     prices: {
-      petite: 6.5,
-      moyenne: 12,
-      grande: 14
+      petite: 7.5,
+      moyenne: 13,
+      grande: 15
     }
   }
 ]
